ed7ade6fa4abea1006ee01ef27df27929bcc2ad1
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-09-14 09:33:46 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+
+const Home: NextPage = () => {
+	return <div> well well well</div>;
+};
+
+export default Home;
