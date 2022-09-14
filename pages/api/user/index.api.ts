eeed55d90fe05699f0713bbf241a886575c3f5e1
0101110101 <51328751+0101110101@users.noreply.github.com>
2022-09-14 16:27:09 +0200

    User api route

--- pages/api/user/index.api.ts
+++ pages/api/user/index.api.ts
@@ -1,5 +1,3 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	res.status(200).json({ name: "John Doe" });
-}
+export default function handler(req: NextApiRequest, res: NextApiResponse) {}
