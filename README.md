21f17b58727d44e766beb11ac24eb81da0ae4214
szkieb <102992385+szkieb@users.noreply.github.com>
2022-09-14 15:46:59 +0200

    Dev (#23)
    * Update README.md
    
    * first commit
    
    * Update README.md
    
    * specified yarn use in ReadMe
    
    * backend is set up
    
    * d
    
    * added storybook
    
    Co-authored-by: 0101110101 <51328751+0101110101@users.noreply.github.com>
    Co-authored-by: franzwollang <franzwollang@gmail.com>
    Co-authored-by: Franz Wollang <franzwollang@Franzs-MacBook-Pro.local>

--- README.md
+++ README.md
@@ -40,3 +40,10 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+## Storybook
+
+Installed the ESLint Plugin for Storybook
+https://github.com/storybookjs/eslint-plugin-storybook#readme
+
+For details and info on Storybook check the introduction file inside the stories folder.
---
+++ components/example-component/Button.tsx
@@ -0,0 +1,63 @@
+import "./button.css";
+import Image from "next/image";
+
+interface ButtonProps {
+	/**
+	 * Is this the principal call to action on the page?
+	 */
+	primary?: boolean;
+	/**
+	 * What background color to use
+	 */
+	backgroundColor?: string;
+	/**
+	 * How large should the button be?
+	 */
+	size?: "small" | "medium" | "large";
+	/**
+	 * Button contents
+	 */
+	label: string;
+	/**
+	 * Optional click handler
+	 */
+	onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Button = ({
+	primary = false,
+	size = "medium",
+	backgroundColor,
+	label,
+	...props
+}: ButtonProps) => {
+	const mode = primary
+		? "storybook-button--primary"
+		: "storybook-button--secondary";
+	return (
+		<>
+			<button
+				type="button"
+				className={[
+					"shadow-xl",
+					"storybook-button",
+					`storybook-button--${size}`,
+					mode,
+				].join(" ")}
+				style={{ backgroundColor }}
+				{...props}
+			>
+				{label}
+			</button>
+			{/* The image is just here to show that the image works. */}
+			<Image
+				width={50}
+				height={50}
+				src="https://ih1.redbubble.net/image.3315780744.1289/pp,504x498-pad,600x600,f8f8f8.jpg"
+			/>
+		</>
+	);
+};
