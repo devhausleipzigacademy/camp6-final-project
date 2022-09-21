591a104b577d08c379b28ae906ff5cfae50b75cf
Franz Wollang <franzwollang@Franzs-MacBook-Pro.local>
2022-09-21 15:25:45 +0200

    Switch over to Vitest

--- pages/api/book/index.api.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect } from "@jest/globals";
-
-describe("Book CRUD Endpoints", () => {
-	it("handler", () => {
-		expect(true).toEqual(true);
-	});
-});
---
+++ pages/api/book/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "vitest";
+
+describe("Book CRUD Endpoints", () => {
+    it("handler", () => {
+        expect(true).toEqual(true);
+    });
+});
