ccfd7d37048634e325a2e0e54e6d3a667a2ba070
Franz Wollang <franzwollang@Franzs-MacBook-Pro.local>
2022-10-04 11:08:13 +0200

    Change host to port 3000

--- utils/fetchBook.ts
+++ utils/fetchBook.ts
@@ -1,7 +1,7 @@
 import { Book } from "@prisma/client";
 
 export function fetchBook(bookId: string): Promise<Book> {
-    return fetch(`http://localhost:3002/api/book/${bookId}`, {
+    return fetch(`http://localhost:3000/api/book/${bookId}`, {
         method: "GET",
     }).then((res) => {
         return res.json();
--- utils/fetchBooks.ts
+++ utils/fetchBooks.ts
@@ -4,13 +4,13 @@ type FetchBookProps = {
     availability?: boolean;
 };
 
-export default function fetchBook({
+export default function fetchBooks({
     category,
     orderBy,
     availability = true,
 }: FetchBookProps) {
     return fetch(
-        `http://localhost:3002/api/book?availability=${availability}&category=${category}&orderBy=${orderBy}`,
+        `http://localhost:3000/api/book?availability=${availability}&category=${category}&orderBy=${orderBy}`,
         {
             method: "GET",
         }
