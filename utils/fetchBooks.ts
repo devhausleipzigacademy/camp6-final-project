b3d87cc77d1d3fa17e4c1fe92d5d587eb863f4fc
stephanr <ritscher@gmx.com>
2022-10-11 11:47:10 +0200

    created Error handler function to avoid site from breaking due to failed fetch requests

--- utils/fetchBooks.ts
+++ utils/fetchBooks.ts
@@ -1,55 +1,56 @@
 const host =
-    process.env.NODE_ENV == "production"
-        ? process.env.NEXT_PUBLIC_PROD_HOST
-        : process.env.NEXT_PUBLIC_DEV_HOST;
+	process.env.NODE_ENV == "production"
+		? process.env.NEXT_PUBLIC_PROD_HOST
+		: process.env.NEXT_PUBLIC_DEV_HOST;
 
 type FetchBookProps = {
-    genre?: any;
-    title?: string;
-    orderBy?: "createdAt" | "title" | "isAvailable";
-    author?: string;
-    language?: string;
-    isAvailable?: boolean;
-    borrowed?: Boolean;
+	genre?: any;
+	title?: string;
+	orderBy?: "createdAt" | "title" | "isAvailable";
+	author?: string;
+	language?: string;
+	isAvailable?: boolean;
+	borrowed?: Boolean;
 };
 
 export default function fetchBooks({
-    isAvailable,
-    author,
-    title,
-    genre,
-    language,
-    borrowed,
+	isAvailable,
+	author,
+	title,
+	genre,
+	language,
+	borrowed,
 }: FetchBookProps) {
-    let URLString = `http://${host}/api/book?`;
+	let URLString = `http://${host}/api/book?`;
 
-    if (isAvailable !== undefined) {
-        URLString += `isAvailable=${isAvailable}&`;
-    }
+	if (isAvailable !== undefined) {
+		URLString += `isAvailable=${isAvailable}&`;
+	}
 
-    if (title !== undefined) {
-        URLString += `title=${title}&`;
-    }
+	if (title !== undefined) {
+		URLString += `title=${title}&`;
+	}
 
-    if (author !== undefined) {
-        URLString += `author=${author}&`;
-    }
+	if (author !== undefined) {
+		URLString += `author=${author}&`;
+	}
 
-    if (language !== undefined) {
-        URLString += `language=${language}&`;
-    }
+	if (language !== undefined) {
+		URLString += `language=${language}&`;
+	}
 
-    if (genre !== undefined) {
-        URLString += `genre=${genre}&`;
-    }
+	if (genre !== undefined) {
+		URLString += `genre=${genre}&`;
+	}
 
-    if (borrowed !== undefined) {
-        URLString += `borrowed=${borrowed}&`;
-    }
+	if (borrowed !== undefined) {
+		URLString += `borrowed=${borrowed}&`;
+	}
 
-    return fetch(URLString, {
-        method: "GET",
-    }).then((res) => {
-        return res.json();
-    });
+	return fetch(URLString, {
+		method: "GET",
+	}).then((res) => {
+		if (!res.ok) throw Error;
+		return res.json();
+	});
 }
