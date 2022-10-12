bdeed2e2fec1f64dca77928ebff2e16ae831534c
lkraem <lisa.kraemers@gmail.com>
2022-10-12 17:37:33 +0200

    extended db with like status

--- pages/library.page.tsx
+++ pages/library.page.tsx
@@ -1,10 +1,10 @@
 // package imports
 import Link from "next/link";
 import {
-    MutationFunction,
-    useMutation,
-    useQuery,
-    useQueryClient,
+  MutationFunction,
+  useMutation,
+  useQuery,
+  useQueryClient,
 } from "@tanstack/react-query";
 
 // local imports
@@ -16,83 +16,83 @@ import { CustomButton } from "../components/button/Button";
 import { updateBook } from "../utils/updateBook";
 
 export default function Library() {
-    const { data: books, isLoading: booksLoading } = useQuery<Book[]>(
-        ["books"],
-        () => fetchBooks({ orderBy: "title" })
-    );
+  const { data: books, isLoading: booksLoading } = useQuery<Book[]>(
+    ["books"],
+    () => fetchBooks({ orderBy: "title" })
+  );
 
-    if (booksLoading) return <p>Loading...</p>;
+  if (booksLoading) return <p>Loading...</p>;
 
-    if (!booksLoading && books === undefined) return <p>no books found</p>;
+  if (!booksLoading && books === undefined) return <p>no books found</p>;
 
-    return (
-        <>
-            <div className="fixed top-[88vh] right-4">
-                <CustomButton
-                    functionality={"AddBook"}
-                    onClick={function (): void {
-                        throw new Error("Function not implemented.");
-                    }}
-                ></CustomButton>
-            </div>
-            <h2 className="pageTitle">
-                Library
-                <Link href="/loans">
-                    <a className="pl-12 text-grey">Loans at a Glance</a>
-                </Link>
-            </h2>
-            {books.map((book) => (
-                <div id={book.identifier}>
-                    <LibraryItem key={book.identifier} book={book} />
-                </div>
-            ))}
-        </>
-    );
+  return (
+    <>
+      <div className="fixed top-[88vh] right-4">
+        <CustomButton
+          functionality={"AddBook"}
+          onClick={function (): void {
+            throw new Error("Function not implemented.");
+          }}
+        ></CustomButton>
+      </div>
+      <h2 className="pageTitle">
+        Library
+        <Link href="/loans">
+          <a className="pl-12 text-grey">Loans at a Glance</a>
+        </Link>
+      </h2>
+      {books.map((book) => (
+        <div id={book.identifier}>
+          <LibraryItem key={book.identifier} book={book} />
+        </div>
+      ))}
+    </>
+  );
 }
 
 interface LibraryItemProps {
-    book: Book;
+  book: Book;
 }
 
 function LibraryItem({ book }: LibraryItemProps) {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    const mutation = useMutation(updateBook, {
-        onSuccess: () => {
-            queryClient.invalidateQueries(["books"]);
-        },
-    });
+  const mutation = useMutation(updateBook, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["books"]);
+    },
+  });
 
-    return (
-        <div className="flex cursor-pointer justify-evenly border-b-0.75 border-grey p-5">
-            <BookPreview
-                isAvailable={book.isAvailable}
-                bookTitle={book.title}
-                bookAuthor={book.author}
-                imgSrc={book.image}
-                linkHref={`/book/${book.identifier}`}
-                bookSize={"listItemSmall"}
-            />
-            <Link href={`/book/${book.identifier}`}>
-                <a className="ml-7 flex w-full flex-col justify-center font-montserrat text-sm font-normal text-textBlack">
-                    <p>{book.author}</p>
-                    <p>{book.title}</p>
-                </a>
-            </Link>
-            <div className="flex items-center gap-3">
-                {/* TODO: add functionality to Edit Button */}
-                {/* <FiEdit className="text-brown" /> */}
+  return (
+    <div className="flex cursor-pointer justify-evenly border-b-0.75 border-grey p-5">
+      <BookPreview
+        isAvailable={book.isAvailable}
+        bookTitle={book.title}
+        bookAuthor={book.author}
+        imgSrc={book.image}
+        linkHref={`/book/${book.identifier}`}
+        bookSize={"listItemSmall"}
+      />
+      <Link href={`/book/${book.identifier}`}>
+        <a className="ml-7 flex w-full flex-col justify-center font-montserrat text-sm font-normal text-textBlack">
+          <p>{book.author}</p>
+          <p>{book.title}</p>
+        </a>
+      </Link>
+      <div className="flex items-center gap-3">
+        {/* TODO: add functionality to Edit Button */}
+        {/* <FiEdit className="text-brown" /> */}
 
-                <ToggleSwitch
-                    value={book.isAvailable}
-                    toggleHandler={() =>
-                        mutation.mutate({
-                            bookId: book.identifier,
-                            book: { isAvailable: !book.isAvailable },
-                        })
-                    }
-                ></ToggleSwitch>
-            </div>
-        </div>
-    );
+        <ToggleSwitch
+          value={book.isAvailable}
+          toggleHandler={() =>
+            mutation.mutate({
+              bookId: book.identifier,
+              book: { isAvailable: !book.isAvailable },
+            })
+          }
+        ></ToggleSwitch>
+      </div>
+    </div>
+  );
 }
--- utils/fetchBooks.ts
+++ utils/fetchBooks.ts
@@ -1,55 +1,61 @@
 const host =
-    process.env.NODE_ENV == "production"
-        ? process.env.NEXT_PUBLIC_PROD_HOST
-        : process.env.NEXT_PUBLIC_DEV_HOST;
+  process.env.NODE_ENV == "production"
+    ? process.env.NEXT_PUBLIC_PROD_HOST
+    : process.env.NEXT_PUBLIC_DEV_HOST;
 
 type FetchBookProps = {
-    genre?: any;
-    title?: string;
-    orderBy?: "createdAt" | "title" | "isAvailable";
-    author?: string;
-    language?: string;
-    isAvailable?: boolean;
-    borrowed?: Boolean;
+  genre?: any;
+  title?: string;
+  orderBy?: "createdAt" | "title" | "isAvailable";
+  author?: string;
+  language?: string;
+  isAvailable?: boolean;
+  borrowed?: boolean;
+  likedBy?: string;
 };
 
 export default function fetchBooks({
-    isAvailable,
-    author,
-    title,
-    genre,
-    language,
-    borrowed,
+  isAvailable,
+  author,
+  title,
+  genre,
+  language,
+  borrowed,
+  likedBy,
 }: FetchBookProps) {
-    let URLString = `http://${host}/api/book?`;
+  let URLString = `http://${host}/api/book?`;
 
-    if (isAvailable !== undefined) {
-        URLString += `isAvailable=${isAvailable}&`;
-    }
+  if (isAvailable !== undefined) {
+    URLString += `isAvailable=${isAvailable}&`;
+  }
 
-    if (title !== undefined) {
-        URLString += `title=${title}&`;
-    }
+  if (title !== undefined) {
+    URLString += `title=${title}&`;
+  }
 
-    if (author !== undefined) {
-        URLString += `author=${author}&`;
-    }
+  if (author !== undefined) {
+    URLString += `author=${author}&`;
+  }
 
-    if (language !== undefined) {
-        URLString += `language=${language}&`;
-    }
+  if (language !== undefined) {
+    URLString += `language=${language}&`;
+  }
 
-    if (genre !== undefined) {
-        URLString += `genre=${genre}&`;
-    }
+  if (genre !== undefined) {
+    URLString += `genre=${genre}&`;
+  }
 
-    if (borrowed !== undefined) {
-        URLString += `borrowed=${borrowed}&`;
-    }
+  if (borrowed !== undefined) {
+    URLString += `borrowed=${borrowed}&`;
+  }
 
-    return fetch(URLString, {
-        method: "GET",
-    }).then((res) => {
-        return res.json();
-    });
+  if (likedBy !== undefined) {
+    URLString += `likedBy=${likedBy}&`;
+  }
+
+  return fetch(URLString, {
+    method: "GET",
+  }).then((res) => {
+    return res.json();
+  });
 }
