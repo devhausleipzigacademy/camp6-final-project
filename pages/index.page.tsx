a2e958008a42394cb7b5302fe975d0fb265bf0ad
Stephan <102992385+szkieb@users.noreply.github.com>
2022-10-12 21:52:00 +0200

    created Error handler function to avoid site from breaking due to failed fetch requests (#61)

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -10,88 +10,108 @@ import { Book } from "@prisma/client";
 import { HomeSearchBar } from "../components/SearchBars/HomeSearchbar/HomeSearchBar";
 import { useState } from "react";
 import SubHeading2 from "../components/Subheading/Subheading";
+import checkQuery from "../utils/checkQuery";
 
 export interface SearchParams {
-    query: string;
-    zipCode: string;
-    languages: {
-        English: boolean;
-        German: Boolean;
-        French: boolean;
-    };
+	query: string;
+	zipCode: string;
+	languages: {
+		English: boolean;
+		German: Boolean;
+		French: boolean;
+	};
 }
 
 const initialSearchParams: SearchParams = {
-    query: "",
-    zipCode: "",
-    languages: {
-        English: false,
-        German: false,
-        French: false,
-    },
+	query: "",
+	zipCode: "",
+	languages: {
+		English: false,
+		German: false,
+		French: false,
+	},
 };
 
 const Home: NextPage = (props) => {
-    const [searchParams, setSearchParams] = useState(initialSearchParams);
-    const genres = ["Cookbook", "Fiction"];
+	const [searchParams, setSearchParams] = useState(initialSearchParams);
+	const genres = ["Cookbook", "Fiction"];
 
-    const categoryData = Object.fromEntries(
-        genres.map((genre) => [
-            genre,
-            useQuery<Book[]>(["books", genre], () =>
-                fetchBooks({
-                    orderBy: "createdAt",
-                    isAvailable: true,
-                    genre,
-                })
-            ),
-        ])
-    );
+	const categoryData = Object.fromEntries(
+		genres.map((genre) => [
+			genre,
+			useQuery<Book[]>(["books", genre], () =>
+				fetchBooks({
+					orderBy: "createdAt",
+					isAvailable: true,
+					genre,
+				})
+			),
+		])
+	);
 
-    const recentUploadsQuery = useQuery<Book[]>(["getBooks", "createdAt"], () =>
-        fetchBooks({ orderBy: "createdAt", isAvailable: true })
-    );
+	const {
+		data: recentUploadsQuery,
+		status: queryStatus,
+		error,
+	} = useQuery<Book[]>(["getBooks", "createdAt"], () =>
+		fetchBooks({ orderBy: "createdAt", isAvailable: true })
+	);
 
-    console.log(recentUploadsQuery.isError, recentUploadsQuery.data);
+	checkQuery({
+		queryStatus: queryStatus,
+		queryItem: recentUploadsQuery,
+		queryName: "book",
+	});
 
-    return (
-        <>
-            <HomeSearchBar
-                searchParams={searchParams}
-                setSearchParams={setSearchParams}
-            />
-            <div className="pl-6">
-                <section id="carousel">
-                    <div key="0">
-                        <SubHeading2>Recent Uploads</SubHeading2>
-                        {recentUploadsQuery.isError ? (
-                            <p>Something went wrong...</p>
-                        ) : recentUploadsQuery.isLoading ? (
-                            <p>Loading...</p>
-                        ) : (
-                            <Carousel books={recentUploadsQuery.data} />
-                        )}
-                    </div>
-                    {Object.entries(categoryData).map(
-                        ([category, query], index) => {
-                            return (
-                                <div key={index + 1}>
-                                    <SubHeading2>{category}</SubHeading2>
-                                    {query.isError ? (
-                                        <p>Something went wrong...</p>
-                                    ) : query.isLoading ? (
-                                        <p>Loading...</p>
-                                    ) : (
-                                        <Carousel books={query.data} />
-                                    )}
-                                </div>
-                            );
-                        }
-                    )}
-                </section>
-            </div>
-        </>
-    );
+	if (queryStatus === "loading") {
+		return <p className="p-6 font-montserrat text-textGrey">Loading...</p>;
+	}
+
+	if (queryStatus === "error") {
+		return (
+			<div className="flex h-screen w-full flex-col items-center justify-center">
+				<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFgCFmiB0pQYHGDklq0EZX35hq-EuOP7N8Xg&usqp=CAU" />
+				<p className="font-montserrat text-textGrey">
+					Something went wrong. Please try again later.
+				</p>
+			</div>
+		);
+	}
+
+	if (recentUploadsQuery.length === 0) {
+		return <p className="p-6 font-montserrat text-textGrey">No books found.</p>;
+	}
+
+	return (
+		<>
+			<HomeSearchBar
+				searchParams={searchParams}
+				setSearchParams={setSearchParams}
+			/>
+			<div className="pl-6">
+				<section id="carousel">
+					<div key="0">
+						<SubHeading2>Recent Uploads</SubHeading2>
+
+						<Carousel books={recentUploadsQuery} />
+					</div>
+					{Object.entries(categoryData).map(([category, query], index) => {
+						checkQuery({
+							queryItem: query.data,
+							queryName: "books",
+							queryStatus: query.status,
+						});
+						return (
+							<div key={index + 1}>
+								<SubHeading2>{category}</SubHeading2>
+								<Carousel books={query.data} />
+							</div>
+						);
+					})}
+				</section>
+			</div>
+		</>
+	);
 };
 
 export default Home;
